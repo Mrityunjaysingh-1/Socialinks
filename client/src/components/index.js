e06cc42ad250b4eb569dd7301157db411fc66800
Mrityunjay Singh <mrityunjay.singh119@gmail.com>
2023-12-29 18:19:49 +0530

    FrontEnd Client Side Code Done

---
+++ client/src/components/index.js
@@ -0,0 +1,19 @@
+import TextInput from "./TextInput";
+import Loading from "./Loading";
+import CustomButton from "./CustomButton";
+import TopBar from "./TopBar";
+import FriendsCard from "./FriendsCard";
+import ProfileCard from "./ProfileCard";
+import PostCard from "./PostCard";
+import EditProfile from "./EditProfile";
+
+export {
+  ProfileCard,
+  EditProfile,
+  PostCard,
+  FriendsCard,
+  TopBar,
+  Loading,
+  TextInput,
+  CustomButton,
+};
